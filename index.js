2be3ae31a91d5f28ddcc43fbf47a311d89b28fe5
erlanggadwi <64958442+erlanggadwi@users.noreply.github.com>
2022-01-13 22:30:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -936,7 +936,8 @@ ${
         }*/
         break;
         case "pet":
-        if (!isQuotedImage && !isImage)
+		    		              reply("Fitur Maintenance");
+         /*if (!isQuotedImage && !isImage)
           return reply(`Reply/Kirim image Dengan Caption ${command}`);
         jarsw3 = isQuotedImage
           ? JSON.parse(JSON.stringify(lin).replace("quotedM", "m")).message
@@ -947,7 +948,7 @@ ${
         buffertt = await getBuffer(
           `https://erdwpe-api.herokuapp.com/creator/pet?avatarURL=${res34}`
         );
-        erdwpe.sendSticker(from, buffertt, lin);
+        erdwpe.sendSticker(from, buffertt, lin);*/
         break;
       case "triggered":
 		              reply("Fitur Maintenance");
